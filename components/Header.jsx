1899d8131a9ca8bde81d3a70d5f73c61f2efb33f
Mona Lisa <ballboss6183@gmail.com>
2023-07-19 17:14:46 +0700

    okkk

---
+++ components/Header.jsx
@@ -0,0 +1,10 @@
+export const Header = (props) => {
+  return (
+    <div>
+      <h4 className="display-4 text-center fst-italic">Todo List Mock Up</h4>
+      <p className="text-secondary text-center">
+        This lab teach you how to split react component(s)
+      </p>
+    </div>
+  );
+};
